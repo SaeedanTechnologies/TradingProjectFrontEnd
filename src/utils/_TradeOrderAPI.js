b223f64ca7a3b8b73259312a0705a35ef32b84ba
MUHAMMAD RAUF ARSHAD <rauf@saeedantechpvt.com>
2024-05-16 17:03:09 +0500

    1 Minute Chart

--- src/utils/_TradeOrderAPI.js
+++ src/utils/_TradeOrderAPI.js
@@ -16,6 +16,13 @@ export const GET_Group_Transaction_Order = async (token, trading_group_id) => {
     return res
 }
 
+export const GET_Min_Chart_Data = async (token, page = 1, perPage = 10, searchValues) => {
+    const queryParams = new URLSearchParams(searchValues).toString();
+    const apiUrlWithParams = `${apiUrl}/admin/charts?page=${page}&per_page=${perPage}&${queryParams}`;
+    const res = await _API(apiUrlWithParams, 'get', [], token);
+    return res;
+}
+
 export const Save_Group_Order = async (TransactionOrderGroupData, trading_group_id, token) => {
     const res = await _API(`${apiUrl}/admin/group_transaction_orders?trading_group_id=${trading_group_id}`, 'post', TransactionOrderGroupData, token)
     return res
