139d0aedf19571d2f8d2e190c5a324a88035b051
Daniyal1303 <158044875+Daniyalahmadsaeedantechpvt@users.noreply.github.com>
2024-05-24 18:04:24 +0500

    pull request and conflict resolve from Mudassar Branch

--- src/Pages/TradingAccount/TradingAccountsEntry.jsx
+++ src/Pages/TradingAccount/TradingAccountsEntry.jsx
@@ -2,7 +2,6 @@ import { theme, Spin, Dropdown } from 'antd';
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { LeftOutlined, RightOutlined, EllipsisOutlined } from '@ant-design/icons';
-
 import * as Yup from 'yup';
 import ARROW_BACK_CDN from '../../assets/images/arrow-back.svg'
 import CustomTextField from '../../components/CustomTextField';
