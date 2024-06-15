a2890a5e394849bbc766e348adfe6a40ee1b99a2
Daniyal1303 <158044875+Daniyalahmadsaeedantechpvt@users.noreply.github.com>
2024-06-15 19:27:47 +0500

    duplicates changes

--- src/Pages/DnDTable/CSVStepper/DuplicateHandling.jsx
+++ src/Pages/DnDTable/CSVStepper/DuplicateHandling.jsx
@@ -15,31 +15,36 @@ const DuplicateHandling = () => {
     const [enable_password_change,setEnable_password_change] = useState(0);
     const [enable_investor_trading,setEnable_investor_trading] = useState(0);
     const [change_password_at_next_login,setChange_password_at_next_login] = useState(0)
-    const [forwardArrowToggle,setForwardArrowToggle] = useState(true)
-    const [backArrowToggle,setBackArrowToggle] = useState(false)
 
 
- const ChkBoxesControlLeft = [
+
+    const ChkBoxesControl = [
   {
     id:5,
     control:'CustomCheckbox',
     label:'Enable This Account',
     value:enable,
-    key:'enable'
+    key:'enable',
+    left:true,
+    right:false
   },
   {
     id:6,
     control:'CustomCheckbox',
     label:'Enable Password Change',
     value:enable_password_change,
-    key:'enable_password_change'
+    key:'enable_password_change',
+    left:true,
+    right:false
   },
   {
     id:7,
     control:'CustomCheckbox',
     label:'Enable Investor Trading',
     value:enable_investor_trading,
-    key:'enable_investor_trading'
+    key:'enable_investor_trading',
+    left:true,
+    right:false
 
   },
   {
@@ -47,57 +52,52 @@ const DuplicateHandling = () => {
     control:'CustomCheckbox',
     label:'Change Password at Next Login',
     value:change_password_at_next_login,
-    key:'change_password_at_next_login'
+    key:'change_password_at_next_login',
+    left:true,
+    right:false
   },
-    ]
+  ]
 
-    const [chkBoxesControlLeft,setChkBoxesControlLeft] = useState(ChkBoxesControlLeft)
-    const [chkBoxesControlRight,setChkBoxesControlRight] = useState([])
+  // console.log('enable',enable)
 
 
-   
+  const [chkBoxesControl,setChkBoxesControl] = useState(ChkBoxesControl)
 
   const handleInputChange = (fieldName, value) => {
+    debugger;
 
     switch (fieldName) {
-      case 'enable':
+         
+          case 'enable':
           setEnable(value) 
+          break;
          
-        break;
-        case 'enable_password_change':
-         setEnable_password_change(value)
-         
-         break;
-        case 'enable_investor_trading':
-         setEnable_investor_trading(value) 
-         
-        break;
-        case 'change_password_at_next_login':
-        setChange_password_at_next_login(value)
+          case 'enable_password_change':
+          setEnable_password_change(value)
+          break;
         
-        break;
+          case 'enable_investor_trading':
+          setEnable_investor_trading(value) 
+          break;
+
+          case 'change_password_at_next_login':
+          setChange_password_at_next_login(value)
+          break;
 
     }
   };
 
   const handleForward = () => {
-    const checkedLeftItems = chkBoxesControlLeft.filter(item => item.value);
-  const updatedLeftArray = chkBoxesControlLeft.filter(item => !item.value);
-  const updatedRightArray = [...chkBoxesControlRight, ...checkedLeftItems];
-
-  setChkBoxesControlLeft(updatedLeftArray);
-  setChkBoxesControlRight(updatedRightArray);
+    const updatedControlArray = chkBoxesControl.map(x=>({...x,left: value ? false :true,right:value ? true : false }))
+    setChkBoxesControl(updatedControlArray);
+  
 };
 
  const handleBack = () => {
     
- const checkedRightItems = chkBoxesControlRight.filter(item => item.value);
-  const updatedRightArray = chkBoxesControlRight.filter(item => !item.value);
-  const updatedLeftArray = [...chkBoxesControlLeft, ...checkedRightItems];
-
-  setChkBoxesControlLeft(updatedLeftArray);
-  setChkBoxesControlRight(updatedRightArray); 
-
+     const updatedControlArray = chkBoxesControl.map(x=>({...x,right: value ? false :true,left:value ? true : false }))
+    setChkBoxesControl(updatedControlArray);
+  
 };
 
     
@@ -128,7 +128,7 @@ const DuplicateHandling = () => {
                     
                     <Stack direction="row" justifyContent={"space-between"} sx={{width:"70%"}}>
                         <div className='bg-white shadow-md w-[300px] py-6 px-4'>
-                            {chkBoxesControlLeft?.map(item=><CustomCheckbox key={item?.id} label={item?.label} 
+                            {chkBoxesControl?.map(item=> item.left && <CustomCheckbox key={item?.id} label={item?.label} 
                             checked={item?.value} onChange={(event)=> handleInputChange(item?.key, event.target.checked)}
                             /> )}
                         </div>
@@ -147,19 +147,15 @@ const DuplicateHandling = () => {
                         </Stack>
 
                         <div className='bg-white shadow-md w-[300px]  py-6 px-4'>
-                            {chkBoxesControlRight?.map(item=>  <CustomCheckbox key={item?.id} label={item?.label} 
+                            {chkBoxesControl?.map(item=> item.right && <CustomCheckbox key={item?.id} label={item?.label} 
                             checked={item?.value} onChange={(event)=> handleInputChange(item?.key, event.target.checked)}
                             /> )}
 
                         </div>
-                    </Stack>
-                    
+                    </Stack>                  
                 </Stack> 
-
             </Stack>
              
-
-
         </Stack> 
 
 
